4420d9eab30a03c51c90f9b7f0bc6a4521945f9e
ye2519120 <yechengjun@vpgame.cn>
2018-01-12 16:47:26 +0800

    init

---
+++ examples/async-data/app.js
@@ -0,0 +1,59 @@
+import React from 'react'
+import DOM from 'react-dom'
+import Autocomplete from '../../lib/index'
+import { getStates, matchStateToTerm, sortStates, styles, fakeRequest } from '../../lib/utils'
+
+let App = React.createClass({
+
+  getInitialState () {
+    return {
+      value: '',
+      unitedStates: getStates(),
+      loading: false
+    }
+  },
+
+  render () {
+    return (
+      <div>
+        <h1>Async Data</h1>
+        <p>
+          Autocomplete works great with async data by allowing you to pass in
+          items. The <code>onChange</code> event provides you the value to make
+          a server request with, then change state and pass in new items, it will
+          attempt to autocomplete the first one.
+        </p>
+        <label htmlFor="states-autocomplete">Choose a state from the US</label>
+        <Autocomplete
+          inputProps={{name: "US state", id: "states-autocomplete"}}
+          ref="autocomplete"
+          value={this.state.value}
+          items={this.state.unitedStates}
+          getItemValue={(item) => item.name}
+          onSelect={(value, item) => {
+            // set the menu to only the selected item
+            this.setState({ value, unitedStates: [ item ] })
+            // or you could reset it to a default list again
+            // this.setState({ unitedStates: getStates() })
+          }}
+          onChange={(event, value) => {
+            this.setState({ value, loading: true })
+            fakeRequest(value, (items) => {
+              this.setState({ unitedStates: items, loading: false })
+            })
+          }}
+          renderItem={(item, isHighlighted) => (
+            <div
+              style={isHighlighted ? styles.highlightedItem : styles.item}
+              key={item.abbr}
+              id={item.abbr}
+            >{item.name}</div>
+          )}
+        />
+      </div>
+    )
+  }
+})
+
+DOM.render(<App/>, document.getElementById('container'))
+
---
+++ examples/static-data/app.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import DOM from 'react-dom'
+import { getStates, matchStateToTerm, sortStates, styles } from '../../lib/utils'
+import Autocomplete from '../../lib/index'
+
+let App = React.createClass({
+  getInitialState() {
+    return { value: 'Ma' }
+  },
+  render () {
+    return (
+      <div>
+        <h1>Basic Example with Static Data</h1>
+        <p>
+          When using static data, you use the client to sort and filter the items,
+          so <code>Autocomplete</code> has methods baked in to help.
+        </p>
+        <label htmlFor="states-autocomplete">Choose a state from the US</label>
+        <Autocomplete
+          value={this.state.value}
+          inputProps={{name: "US state", id: "states-autocomplete"}}
+          items={getStates()}
+          getItemValue={(item) => item.name}
+          shouldItemRender={matchStateToTerm}
+          sortItems={sortStates}
+          onChange={(event, value) => this.setState({ value })}
+          onSelect={value => this.setState({ value })}
+          renderItem={(item, isHighlighted) => (
+            <div
+              style={isHighlighted ? styles.highlightedItem : styles.item}
+              key={item.abbr}
+            >{item.name}</div>
+          )}
+        />
+      </div>
+    )
+  }
+})
+
+DOM.render(<App/>, document.getElementById('container'))
+
